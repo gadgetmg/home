f3c805b97a310b2ffab3eb5b0faa3c2ed63edd76
Matt Graham <gadgetmg@pm.me>
2024-01-20 11:40:29 -0500

    dev: Re-enable kustomize load-restrictions

--- hack/dev-cluster.sh
+++ hack/dev-cluster.sh
@@ -5,10 +5,10 @@ cd "$(dirname "$0")"
 kind create cluster --config=kind-config.yaml
 
 # install cilium CNI less custom resources (cilium-operator is responsible for installing CRDs)
-kustomize build --enable-alpha-plugins --enable-exec --enable-helm --load-restrictor LoadRestrictionsNone ../system/cilium/dev | kfilt -x kind=CiliumLoadBalancerIPPool -x kind=CiliumL2AnnouncementPolicy -x kind=GatewayClass | kubectl apply -f -
+kustomize build --enable-alpha-plugins --enable-exec --enable-helm ../system/cilium/dev | kfilt -x kind=CiliumLoadBalancerIPPool -x kind=CiliumL2AnnouncementPolicy -x kind=GatewayClass | kubectl apply -f -
 
 # bootstrap Argo CD CRDs
-kustomize build --enable-alpha-plugins --enable-exec --enable-helm --load-restrictor LoadRestrictionsNone ../system/argocd/dev | kfilt -i kind=CustomResourceDefinition | kubectl apply -f -
+kustomize build --enable-alpha-plugins --enable-exec --enable-helm ../system/argocd/dev | kfilt -i kind=CustomResourceDefinition | kubectl apply -f -
 
 # bootstrap Argo CD
-kustomize build --enable-alpha-plugins --enable-exec --enable-helm --load-restrictor LoadRestrictionsNone ../system/argocd/dev | kfilt -x kind=CustomResourceDefinition | kubectl apply -f -
+kustomize build --enable-alpha-plugins --enable-exec --enable-helm ../system/argocd/dev | kfilt -x kind=CustomResourceDefinition | kubectl apply -f -
