f3ed5b419a3081a39af5670b7cb940d52259a37a
Matt Graham <gadgetmg@pm.me>
2024-02-02 18:54:14 -0500

    Add act and dagger CI

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs = { url = github:NixOS/nixpkgs/nixos-unstable; };
     talhelper = { url = "github:budimanjojo/talhelper"; };
     krewfile = { url = github:brumhard/krewfile; };
+    dagger = { url = github:dagger/nix; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: let
@@ -12,6 +13,7 @@
     pkgs = import nixpkgs { inherit system; };
     krewfile = inputs.krewfile.packages.${system}.default;
     talhelper = inputs.talhelper.packages.${system}.default;
+    dagger = inputs.dagger.packages.${system}.dagger;
     kubernetes-helm = pkgs.kubernetes-helm.overrideAttrs (prev: {
       ldflags = prev.ldflags ++ [
         "-X helm.sh/helm/v3/pkg/chartutil.k8sVersionMajor=1"
@@ -37,6 +39,8 @@
           age
           argocd
           cilium-cli
+          dagger
+          go
           kfilt
           kind
           krew
