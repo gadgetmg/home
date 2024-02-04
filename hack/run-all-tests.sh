84adf4c9144b03f64cc03372f98b355f289b44be
Matt Graham <gadgetmg@pm.me>
2024-02-04 18:39:59 -0500

    test: Revert KUTTL/Dagger implementation (#35)

--- hack/run-all-tests.sh
+++
@@ -1,6 +0,0 @@
-#/usr/bin/env sh
-
-# Find all kuttl-test.yaml files, cd to each, and run kuttl
-find . -iname kuttl-test.yaml |
-    xargs dirname |
-    xargs -i sh -c 'pushd {} && kubectl kuttl test && popd'
