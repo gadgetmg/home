fbebe0504f63d1d8ef50fce8222f072e6bb35d8c
Matt Graham <gadgetmg@pm.me>
2024-02-22 22:55:01 -0500

    feat(dev-cluster): Talos based dev cluster (#70)

--- hack/dev-cluster.sh
+++ hack/dev-cluster.sh
@@ -1,34 +1,48 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 cd "$(dirname "$0")"
 
-# create kind cluster
-kind create cluster --config=kind-config.yaml
-
-# pre-install CRDs
+# renovate: datasource=github-releases depName=siderolabs/talos
+TALOS_VERSION=v1.6.4
+# renovate: datasource=github-releases depName=kubernetes/kubernetes
+KUBERNETES_VERSION=v1.29.2
 # renovate: datasource=helm depName=cilium registryUrl=https://helm.cilium.io
-kubectl apply -f https://github.com/cilium/cilium/raw/v1.15.1/pkg/k8s/apis/cilium.io/client/crds/v2alpha1/ciliumloadbalancerippools.yaml
-# renovate: datasource=helm depName=cilium registryUrl=https://helm.cilium.io
-kubectl apply -f https://github.com/cilium/cilium/raw/v1.15.1/pkg/k8s/apis/cilium.io/client/crds/v2alpha1/ciliuml2announcementpolicies.yaml
-# renovate: datasource=helm depName=cert-manager registryUrl=https://charts.jetstack.io
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.2/cert-manager.crds.yaml
-# renovate: datasource=github-releases depName=prometheus-operator/prometheus-operator
-kubectl apply -f https://github.com/prometheus-operator/prometheus-operator/releases/download/v0.71.2/stripped-down-crds.yaml
-# renovate: datasource=github-releases depName=kubernetes-sigs/gateway-api
-kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/raw/v1.0.0/config/crd/standard/gateway.networking.k8s.io_gatewayclasses.yaml
+CILIUM_VERSION=v1.15.1
 
-# install cilium CNI
-kustomize build --enable-helm ../system/cilium/dev | kubectl apply -f -
+# Bring up Talos cluster
+docker compose up --detach
+# Generate configuration for Talos
+talosctl gen config dev-talos https://10.5.0.2:6443 \
+  --talos-version $TALOS_VERSION \
+  --kubernetes-version $KUBERNETES_VERSION \
+  --config-patch @patches/cilium-cni.yaml \
+  --config-patch @patches/crds.yaml \
+  --config-patch @patches/local-repo.yaml \
+  --config-patch-control-plane @patches/pod-security.yaml \
+  --config-patch-control-plane @patches/schedule-controlplanes.yaml \
+  --output-types controlplane,talosconfig \
+  --output config --force
+# Apply and bootstrap the Talos cluster
+while ! talosctl apply-config --nodes 10.5.0.2 --file config/controlplane.yaml --insecure &> /dev/null; do sleep 1 && echo -n .; done;
+while ! talosctl bootstrap --nodes 10.5.0.2 --endpoints 10.5.0.2 --talosconfig config/talosconfig &> /dev/null; do sleep 1 && echo -n .; done;
+while ! talosctl kubeconfig --nodes 10.5.0.2 --endpoints 10.5.0.2 --talosconfig config/talosconfig &> /dev/null; do sleep 1 && echo -n .; done;
+while ! kubectl wait --for=condition=Ready=false node/dev-controlplane-1 --timeout=600s &> /dev/null; do sleep 1 && echo -n .; done; echo
+# Patch CoreDNS configuration to resolve .dev.local domains with k8s_gateway
+kubectl apply -f local-dns.yaml
+kubectl rollout restart deployment.apps/coredns -n kube-system
 
-# install cert-manager
-kustomize build --enable-helm ../system/cert-manager/dev | kubectl apply -f -
-
-# install Argo CD
-kustomize build ../system/argocd/dev | kubectl apply -f -
-
+cd ..
+# Install Cilium CNI
+kubectl create namespace monitoring
+kustomize build --enable-helm system/cilium/dev | kfilt -K CiliumLoadBalancerIPPool -K CiliumL2AnnouncementPolicy | kubectl apply -f -
+kubectl wait --for=condition=Available deployment.apps/cilium-operator -n kube-system --timeout=10m
+kustomize build --enable-helm system/cilium/dev | kfilt -k CiliumLoadBalancerIPPool -k CiliumL2AnnouncementPolicy | kubectl apply -f -
+kubectl wait --for=condition=Ready nodes --all --timeout=600s
+# Install Argo CD
+kubectl apply --server-side -k system/argocd/dev
+kubectl config set-context --namespace=argocd --current
+kubectl wait --for=condition=Available deployment.apps/argocd-repo-server --timeout=10m
 # Install root app
-jsonnet -J ../vendor ../root/dev/main.jsonnet -y | kubectl apply -f -
-
+jsonnet -J vendor root/dev/main.jsonnet -y | kubectl apply -f -
 # Wait for Argo CD initial password
-kubectl config set-context --namespace='argocd' --current
 kubectl wait --for=condition=Available deployment.apps/argocd-server --timeout=10m
 argocd --core admin initial-password
