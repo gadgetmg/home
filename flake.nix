35339e8ebacc9e52a4c9efd8eaef12ca373f5f15
Matt Graham <gadgetmg@pm.me>
2025-01-20 18:37:12 -0500

    ci(pre-commit): Add CI job for pre-commit (#483)

--- flake.nix
+++ flake.nix
@@ -2,79 +2,94 @@
   description = "Home";
 
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
-    talhelper = {
-      url = "github:budimanjojo/talhelper";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    krewfile = {
-      url = "github:brumhard/krewfile";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    talhelper.url = "github:budimanjojo/talhelper";
+    krewfile.url = "github:brumhard/krewfile";
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      krewfile = inputs.krewfile.packages.${system}.default;
-      talhelper = inputs.talhelper.packages.${system}.default;
-      kustomize-sops = pkgs.kustomize-sops.overrideAttrs ({
-        installPhase = ''
-          mkdir -p $out/bin
-          mv $GOPATH/bin/kustomize-sops $out/bin/ksops
-        '';
-      });
-    in
-    rec {
-      # Accessible via 'nix develop' or 'nix shell'
-      packages.${system} = {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            age
-            argocd
-            cilium-cli
-            crossplane-cli
-            go
-            go-jsonnet
-            go-task
-            hubble
-            jsonnet-bundler
-            k9s
-            kfilt
-            kind
-            krew
-            krewfile
-            kubeconform
-            kubectl
-            kubernetes-helm
-            kubeseal
-            kubevirt
-            kustomize
-            kustomize-sops
-            kyverno-chainsaw
-            pre-commit
-            sops
-            ssh-to-age
-            talhelper
-            talosctl
-            vcluster
-            velero
-            yamlfmt
-            yq-go
-            (python3.withPackages (python-pkgs: with python-pkgs; [
-              pyaml
-              deepdiff
-            ]))
-          ];
-          shellHook = ''
-            export PATH="$HOME/.krew/bin:$PATH"
-            krewfile
-          '';
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          krewfile = inputs.krewfile.packages.${system}.default;
+          talhelper = inputs.talhelper.packages.${system}.default;
+        in
+        {
+          devShells = {
+            ci =
+              with pkgs;
+              mkShell {
+                packages = [
+                  pre-commit
+                  kustomize
+                  kubernetes-helm
+                  kubeconform
+                  go-task
+                ];
+              };
+            default =
+              with pkgs;
+              mkShell {
+                packages = [
+                  age
+                  argocd
+                  cilium-cli
+                  crossplane-cli
+                  go
+                  go-jsonnet
+                  go-task
+                  hubble
+                  jsonnet-bundler
+                  k9s
+                  kfilt
+                  kind
+                  krew
+                  krewfile
+                  kubeconform
+                  kubectl
+                  kubernetes-helm
+                  kubeseal
+                  kubevirt
+                  kustomize
+                  kyverno-chainsaw
+                  pre-commit
+                  sops
+                  ssh-to-age
+                  talhelper
+                  talosctl
+                  vcluster
+                  velero
+                  yamlfmt
+                  yq-go
+                  (python3.withPackages (
+                    python-pkgs: with python-pkgs; [
+                      pyaml
+                      deepdiff
+                    ]
+                  ))
+                ];
+                shellHook = ''
+                  export PATH="$HOME/.krew/bin:$PATH"
+                  krewfile
+                '';
+              };
+          };
         };
-      };
     };
 }
